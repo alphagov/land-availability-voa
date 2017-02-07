ba2ec0e01e3b380b423fe93150e3282d1bd65517
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-02-07 16:38:37 +0000

    Add generic create view for VOA

--- landavailability/voa/views.py
+++ landavailability/voa/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .serializers import PropertySerializer
+from .models import Property
 
-# Create your views here.
+
+class VOACreateView(generics.CreateAPIView):
+    permission_classes = (IsAdminUser, )
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
